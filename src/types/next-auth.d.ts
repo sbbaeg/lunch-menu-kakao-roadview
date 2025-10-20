ec46005d8f048f7cc87afbfe47445f24025819ca
Fortune Teller <fortuneteller@example.com>
2025-10-20 14:35:46 +0900

    불완전했던 타입정의파일 명확하게 수정

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,13 +1,19 @@
-import { DefaultSession } from "next-auth";
+import { DefaultSession, DefaultUser } from "next-auth";
 
 declare module "next-auth" {
   /**
-   * session 객체의 user 타입에 id를 추가합니다.
+   * `session.user` 객체에 `id` 프로퍼티를 추가합니다.
    */
-  interface Session {
-    user?: {
+  interface Session extends DefaultSession {
+    user: {
       id: string;
     } & DefaultSession["user"];
   }
 
+  /**
+   * `user` 객체 (예: `session` 콜백의 `user` 파라미터)에 `id` 프로퍼티를 추가합니다.
+   */
+  interface User extends DefaultUser {
+    id: string;
+  }
 }
