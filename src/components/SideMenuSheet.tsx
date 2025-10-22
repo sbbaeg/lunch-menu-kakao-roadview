6b998419ade950a6c8919ffd24674dffdbede42d
Fortune Teller <fortuneteller@example.com>
2025-10-22 14:31:20 +0900

    h가 아닌 px를 사용해 햄버게 아이콘 확실히 수정

--- src/components/SideMenuSheet.tsx
+++ src/components/SideMenuSheet.tsx
@@ -80,7 +80,7 @@ export function SideMenuSheet({
         <Sheet>
             <SheetTrigger asChild>
                 <Button variant="ghost" className="h-11 w-11">
-                    <Menu className="h-11 w-11" />
+                    <Menu style={{ width: '44px', height: '44px' }} />
                 </Button>
             </SheetTrigger>
             <SheetContent>
