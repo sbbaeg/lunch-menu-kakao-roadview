522f5947f49d13fa7ac054b08e2bc34f7d5a6ea1
Fortune Teller <fortuneteller@example.com>
2025-11-05 11:46:26 +0900

    알림 기능을 포함한 pwa활성화

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 // next.config.mjs
 
-// import withPWA from 'next-pwa'; // PWA 비활성화
+import withPWA from 'next-pwa';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -28,13 +28,11 @@ const nextConfig = {
   },
 };
 
-// PWA is disabled for now
-// const pwaConfig = {
-//   dest: 'public',
-//   register: true,
-//   skipWaiting: true,
-//   disable: process.env.NODE_ENV === 'development',
-// };
+const pwaConfig = {
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
+};
 
-// export default withPWA(pwaConfig)(nextConfig); // PWA 비활성화
-export default nextConfig;
+export default withPWA(pwaConfig)(nextConfig); // PWA 활성화
