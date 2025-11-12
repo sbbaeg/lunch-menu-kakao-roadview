fd77ba5e0288b3cda8025310a29fb05a7fb2069a
Fortune Teller <fortuneteller@example.com>
2025-11-12 15:55:14 +0900

    AppShell 예외처리-관리자페이지

--- src/components/AppShell.tsx
+++ src/components/AppShell.tsx
@@ -1,10 +1,19 @@
 "use client";
 
+import { usePathname } from 'next/navigation';
 import { usePwaDisplayMode } from "@/hooks/usePwaDisplayMode";
 import MobileLayout from "./mobile/MobileLayout";
 
 export function AppShell({ children }: { children: React.ReactNode }) {
+  const pathname = usePathname();
   const { isStandalone } = usePwaDisplayMode();
+
+  const isAdminPage = pathname.startsWith('/admin');
+
+  // 관리자 페이지일 경우, PWA 여부와 상관없이 children을 그대로 렌더링
+  if (isAdminPage) {
+    return <>{children}</>;
+  }
 
   // PWA가 독립 실행 모드일 경우 모바일 UI를 렌더링합니다.
   if (isStandalone) {
