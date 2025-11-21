bc2b11f577b6e8e9bc380460a0e0a47f2a7758e6
Fortune Teller <fortuneteller@example.com>
2025-11-21 09:20:04 +0900

    node.js 스택 크기 옵션 추가 메모리문제 해결

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,26 +1,12 @@
-// src/sw.ts
-/// <reference lib="webworker" />
-import { clientsClaim, setCacheNameDetails, skipWaiting } from 'workbox-core';
-import { precacheAndRoute } from 'workbox-precaching';
+// public/firebase-messaging-sw.js
 
-declare const self: ServiceWorkerGlobalScope & typeof globalThis;
-
-setCacheNameDetails({
-  prefix: 'next-pwa',
-  suffix: 'v1', // 캐시 버전 관리를 위한 접미사
-});
-
-skipWaiting();
-clientsClaim();
-
-// next-pwa가 생성하는 precache manifest를 사용합니다.
-precacheAndRoute(self.__WB_MANIFEST || []);
+// 이 스크립트는 next-pwa가 생성하는 sw.js에 의해 importScripts를 통해 로드됩니다.
 
 // 푸시 알림 이벤트 리스너
 self.addEventListener('push', (event) => {
   console.log('Service Worker: Push received!');
   const data = event.data?.json() || {};
-  const { title, body, icon, badgeCount } = data; // 서버에서 보낼 페이로드 구조에 따라 변경될 수 있습니다.
+  const { title, body, icon, badgeCount } = data;
 
   // 뱃지 카운트 업데이트
   if ('setAppBadge' in navigator && typeof badgeCount === 'number') {
@@ -36,8 +22,6 @@ self.addEventListener('push', (event) => {
     self.registration.showNotification(title || '알림', {
       body: body || '새로운 알림이 도착했습니다.',
       icon: icon || '/icon.png',
-      // 여기에 더 많은 알림 옵션을 추가할 수 있습니다.
-      // 예: vibrate, data, actions, tag
     })
   );
 });
